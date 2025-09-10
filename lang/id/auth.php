b390c7a9228b865756a07ee1a30ada1f86d87e8d
jendrabn <jendra455@gmail.com>
2025-09-10 07:42:18 +0700

    Add Indonesian language support for authentication, pagination, passwords, and validation

---
+++ lang/id/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Baris Bahasa Autentikasi
+    |--------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut digunakan selama proses autentikasi untuk berbagai
+    | pesan yang perlu ditampilkan kepada pengguna. Anda bebas untuk mengubah
+    | baris bahasa ini sesuai dengan kebutuhan aplikasi Anda.
+    |
+    */
+
+    'failed'   => 'Kredensial yang Anda masukkan tidak sesuai dengan data kami.',
+    'password' => 'Kata sandi yang Anda masukkan salah.',
+    'throttle' => 'Terlalu banyak percobaan masuk. Silakan coba lagi dalam :seconds detik.',
+
+];
---
+++ lang/id/pagination.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Baris Bahasa Paginasi
+    |--------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut digunakan oleh pustaka paginasi untuk membangun
+    | tautan paginasi sederhana. Anda bebas mengubahnya sesuai dengan kebutuhan
+    | tampilan aplikasi Anda.
+    |
+    */
+
+    'previous' => '&laquo; Sebelumnya',
+    'next'     => 'Berikutnya &raquo;',
+
+];
